9e2d7faca49f6245850e7a0e14cbc4ab85afd1e8
sm-parin <sm.parin@gmail.com>
2026-06-03 23:44:35 +0000

    web app changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,6 +44,11 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col bg-background text-foreground">
         <Providers>{children}</Providers>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `if('serviceWorker' in navigator){navigator.serviceWorker.register('/sw.js')}`,
+          }}
+        />
       </body>
     </html>
   );
--- app/manifest.ts
+++ app/manifest.ts
@@ -22,6 +22,18 @@ export default function manifest(): MetadataRoute.Manifest {
         sizes: '180x180',
         type: 'image/png',
       },
+      {
+        src: '/apple-icon',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/apple-icon',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
     ],
   };
 }
